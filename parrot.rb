864e7bd0fd615c35b596d2ca17c79e8f69989674
rudi hinds <rudihinds@gmail.com>
2019-04-28 09:37:00 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(S, "Squawk!")
-  puts S
-  return S "Squawk!"
+def parrot(word, "Squawk!")
+  puts word
+  return word "Squawk!"
 end
